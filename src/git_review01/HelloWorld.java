156c6c1dec024b5e71fc74189ac47aa10cac2545
irinayarema <MykolaYarema@yahoo.com>
2024-01-24 21:20:26 -0500

    Hello world.

--- src/git_review01/HelloWorld.java
+++ src/git_review01/HelloWorld.java
@@ -1,9 +1,12 @@
 package git_review01;
 
 public class HelloWorld {
+
     public static void main(String[] args) {
 
         System.out.println("This is Hello World Class");
+
+        System.out.println("Hello Cydeo");
 
     }
 }
